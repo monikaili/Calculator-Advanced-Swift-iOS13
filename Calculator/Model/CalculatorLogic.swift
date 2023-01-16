5a36dbba9a54d4451e74e73d5f9e8995392e438f
monikaili <iliasova.m@gmail.com>
2023-01-16 22:21:31 +0100

    create new model

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Monika Iliašová on 16/01/2023.
+//  Copyright © 2023 London App Brewery. All rights reserved.
+//
+
+import Foundation
